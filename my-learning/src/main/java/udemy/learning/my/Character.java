9161e20a51c42ea3978861b4aa8204fca2c94d9d
Nick Galan <gnick.alan@gmail.com>
2021-08-07 17:13:23 -0400

    Add new class Character with their attributes: life and live

---
+++ my-learning/src/main/java/udemy/learning/my/Character.java
@@ -0,0 +1,14 @@
+package udemy.learning.my;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Character {
+    private float life;
+    private boolean live;
+    // TODO Add class implements
+    // private Armament
+    // priveta Shield
+}
